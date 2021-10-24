930ec0e09f5522a7801608674fbbf06fb274b2c3
root123-root-d <80046885+root123-root-d@users.noreply.github.com>
2021-10-25 01:14:35 +0800

    Update GitTest.java

--- src/main/java/com/zixue/git/GitTest.java
+++ src/main/java/com/zixue/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("master");
         System.out.println("111");
         System.out.println("222");
+        System.out.println("333text");
     }
 }
